4f41ebe715588210084634d9ca6b392d136ff363
Luofan Chen <clfbbn@gmail.com>
2020-12-05 18:21:31 +0800

    [scroll] Updated path
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -3,9 +3,9 @@
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
         --delay 0.1 \
-        --match-command "bash ./spotify_status.sh" \
+        --match-command "bash /home/bbn/.config/polybar/other_scripts/polybar-spotify/spotify_status.sh" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
-        --update-check true '/home/prayag_s/.config/polybar/scripts/get_spotify_status.sh' &
+        --update-check true '/home/bbn/.config/polybar/other_scripts/polybar-spotify/get_spotify_status.sh' &
 
 wait
